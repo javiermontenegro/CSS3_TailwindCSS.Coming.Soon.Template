d7644c6e7fbe23ebb32bc22e6bcc823dfc82685e
javiermontenegro <javiermontenegro.com@gmail.com>
2024-01-31 08:08:05 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+    ],
+    theme: {
+
+        container: {
+            center: true,
+            padding: '1rem',
+        },
+
+        fontFamily: {
+            'body': ['Be Vietnam Pro', 'sans-serif'],
+        },      
+    },
+}
